cf0ef70db4c8aea4466e49efd00d26948de49491
Asiapenguin <nicholaskong@live.ca>
2020-05-13 14:35:50 -0700

    Basic setup

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+  if (message.CONENT == 'PING') {
+    message.channel.send('PONG')
+  }
+})
+
+client.login(process.env.BOT_TOKEN);
